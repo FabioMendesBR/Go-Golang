60bc841344685db71746460db7094576f7b12938
FabioMendesBR <fabio@4zero.com.br>
2020-08-27 13:20:59 -0300

    Push inicial

--- 01-helloworld.go
+++ 01-helloworld.go
@@ -3,12 +3,22 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Println("")
+	fmt.Println("primeiro comando:")
+	fmt.Println("")
 	fmt.Println("Hello World")
+	fmt.Println("")
 	fmt.Println("Olá Mundo")
+	fmt.Println("")
 	fmt.Println("Hola Mundo")
+	fmt.Println("")
 	fmt.Println("Bonjour le monde")
+	fmt.Println("")
 	fmt.Println("Hallo Welt")
+	fmt.Println("")
 	fmt.Println("こんにちは世界 - Kon nichiwa sekai")
+	fmt.Println("")
 	fmt.Println("你好，世界 - Nǐ hǎo, shìjiè")
+	fmt.Println("")
 
 }
